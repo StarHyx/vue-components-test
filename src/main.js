1a76d180524683cd5fbd949672f6668b37e5044f
starhan <450479592@qq.com>
2018-11-29 20:25:18 +0800

    :package: add global element-ui

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import IdeEditor from 'tiny-python-ide/packages/index.js'
+import Vue from "vue";
+import App from "./App.vue";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import IdeEditor from "tiny-python-ide/packages/index.js";
 
-Vue.config.productionTip = false
-Vue.use(IdeEditor)
+Vue.config.productionTip = false;
+Vue.use(Element);
+Vue.use(IdeEditor);
+import ElSearchTablePagination from "el-search-table-pagination";
+
+// Default use axios as HTTP tool
+Vue.use(ElSearchTablePagination);
+
+// or set a custom HTTP tool
+import axios from "axios";
+
+import "./plugins/element.js";
+Vue.use(ElSearchTablePagination, {
+	axios
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: h => h(App)
+}).$mount("#app");
